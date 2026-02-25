092581a85db9bd6ff017b3b9db4954680800afe7
10Mubarak <abdulroheemmubarak289@gmail.com>
2026-02-25 11:31:09 +1000

    file updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ultra-Wears-Vault/', // Change this line
+  base: '/Ultra-Wears-Collections/', // Change this line
 })
